51dee452c3f1d38d085cb539fe1f5565cfc3eaf4
vadi <vadiraja.k@codenation.co.in>
2019-03-16 19:50:26 +0530

    adding dev env

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,10 @@
-var backendBaseUrl = "https://dev-hub-server.k8.devfactory.com"
+if(process.env.ENV === 'development')
+    var backendBaseUrl = "https://dev-hub-server-staging.k8.devfactory.com"
+else
+    var backendBaseUrl = "https://dev-hub-server.k8.devfactory.com"
+
+console.log("backend URl", backendBaseUrl)
+
 var backendAddonsUrl = backendBaseUrl + "/api/addons/"
 var backendImportUrl = backendBaseUrl + "/api/onboard/"
 var adminCheckUrl = backendBaseUrl + "/api/admin_access_check/"
